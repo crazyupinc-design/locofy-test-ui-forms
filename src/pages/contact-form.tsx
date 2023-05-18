34e7f88fea1b5df4a9d642b5a4f266d2cc130998
디자이너 <crazyupdesigner@dijaineoui-iMac.local>
2023-05-18 09:29:19 +0900

    https://www.figma.com/file/Y2GVmBMCVG1KJjWvVlkqqs/locofy-study?type=design&node-id=2-261&t=YyWG86gKTlOd9U46-0

--- src/pages/contact-form.tsx
+++ src/pages/contact-form.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from "react";
+import { TextField } from "@mui/material";
 import styles from "./contact-form.module.css";
 const ContactForm: FunctionComponent = () => {
   return (
@@ -8,31 +9,47 @@ const ContactForm: FunctionComponent = () => {
           <b className={styles.h2GetInTouch}>Get in touch</b>
           <div className={styles.letUsKnow}>Let us know how we can help</div>
         </div>
-        <div className={styles.formWrapper}>
-          <div className={styles.form}>
-            <div className={styles.inputWrapper}>
-              <div className={styles.formBlockLabel}>Name</div>
-              <div className={styles.formTextInput}>
-                <div className={styles.yourFullName}>Your full name</div>
-              </div>
-            </div>
-            <div className={styles.inputWrapper}>
-              <div className={styles.formBlockLabel}>Email</div>
-              <div className={styles.formTextInput1}>
-                <div className={styles.mecompanycom}>me@company.com</div>
-              </div>
-            </div>
-            <div className={styles.inputWrapper}>
-              <div className={styles.formBlockLabel}>Message</div>
-              <div className={styles.formTextarea}>
-                <div className={styles.yourFullName}>Your message...</div>
-              </div>
-            </div>
-            <div className={styles.formButton}>
-              <div className={styles.yourFullName}>Send message</div>
-            </div>
+        <form className={styles.form}>
+          <div className={styles.inputWrapper}>
+            <div className={styles.formBlockLabel}>Name</div>
+            <TextField
+              className={styles.formTextInput}
+              color="primary"
+              variant="outlined"
+              type="text"
+              label="Your full name"
+              placeholder="Your full name"
+              size="medium"
+              margin="none"
+            />
           </div>
-        </div>
+          <div className={styles.textareaWrapper}>
+            <div className={styles.formBlockLabel}>Email</div>
+            <TextField
+              className={styles.formTextInput}
+              color="primary"
+              variant="outlined"
+              type="text"
+              label="me@company.com"
+              placeholder="me@company.com"
+              size="medium"
+              margin="none"
+            />
+          </div>
+          <div className={styles.textareaWrapper}>
+            <div className={styles.formBlockLabel}>Message</div>
+            <input
+              className={styles.formTextarea}
+              type="text"
+              placeholder="Your message..."
+              maxLength={100}
+              required
+            />
+          </div>
+          <button className={styles.formButton}>
+            <div className={styles.sendMessage}>Send message</div>
+          </button>
+        </form>
       </div>
     </div>
   );
